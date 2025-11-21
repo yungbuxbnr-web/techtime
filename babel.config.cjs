09af26340b4b63557f98427d6910b4ff6487993f
natively.dev <api@natively.dev>
2025-11-21 20:26:14 +0000

    fix: Update babel-preset-expo and simplify babel config for Android build

--- babel.config.cjs
+++ babel.config.cjs
@@ -4,31 +4,6 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          extensions: [
-            '.ios.ts',
-            '.android.ts',
-            '.ts',
-            '.ios.tsx',
-            '.android.tsx',
-            '.tsx',
-            '.jsx',
-            '.js',
-            '.json',
-          ],
-          alias: {
-            '@': './',
-            '@components': './components',
-            '@style': './style',
-            '@hooks': './hooks',
-            '@types': './types',
-          },
-        },
-      ],
-      '@babel/plugin-proposal-export-namespace-from',
       'react-native-reanimated/plugin',
     ],
   };
